867e81cb60420533454945452075d6c9ca4a7a7c
Christian Kjaer Laustsen <ckl@codetalk.io>
2019-10-14 22:50:34 +0200

    Clean up code

--- polysemy-fun/README.md
+++ polysemy-fun/README.md
@@ -5,13 +5,14 @@ This is an exploration in the usage of Polysemy, the latest and greatest Freer e
 
 ### Setting up an Environment
 
+Inside the project:
+```bash
+$ stack build --copy-compiler-tool brittany
+```
+
+Somewhere, do:
 ```bash
 $ git clone git@github.com:digital-asset/ghcide.git \
    && cd ghcide \
    && stack --stack-yaml stack88.yaml build --copy-compiler-tool
-```
-
-Inside the project:
-```bash
-$ stack build --copy-compiler-tool brittany
 ```
--- polysemy-fun/src/Lib.hs
+++ polysemy-fun/src/Lib.hs
@@ -2,18 +2,19 @@ module Lib where
 
 import Polysemy
 
+-- Our effects.
 data Teletype m a where
   ReadTTY  :: Teletype m String
   WriteTTY :: String -> Teletype m ()
-
 makeSem ''Teletype
 
+-- Our interpreter.
 runIOTeletype :: Member (Embed IO) r => Sem (Teletype ': r) a -> Sem r a
 runIOTeletype = interpret $ \case
   ReadTTY      -> embed getLine
   WriteTTY msg -> embed $ putStrLn msg
 
-
+-- Our program.
 echo :: Member Teletype r => Sem r ()
 echo = do
   i <- readTTY
